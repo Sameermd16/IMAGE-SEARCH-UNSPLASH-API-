b18a9849e514519b54a15768d2bebc46e8a06353
sameermd16 <sameerthemc@gmail.com>
2023-08-16 00:42:43 +0530

    deleted callback, added prev & next buttons

--- src/App.jsx
+++ src/App.jsx
@@ -12,40 +12,37 @@ function App() {
   console.log(images)
   const [input, setInput] = useState('')
   const [page, setPage] = useState(1)
-  // const [totalPages, setTotalPages] = useState(0)
+  console.log(page)
+  const [totalPages, setTotalPages] = useState(0)
   const [errorMsg, setErrorMsg] = useState('')
-  console.log(input)
-  // const searchBtn = useRef()
-  // console.log(searchBtn.current)
-
+  
   function handleSearch(event) {
     event.preventDefault()
+    getImages() 
   }
 
-  const fetchImages = useCallback(async () => {
-    try{
-      if(input) {
-        setErrorMsg('')
-        const {data} = await axios.get(`${API_URL}?query=${input}&page=${page}&client_id=${accessKey}`)
-        console.log(data.results)
-        setImages(data.results)
-        // setTotalPages(data.total_pages)
-      }
-    }catch(err) {
-      setErrorMsg("Error handling images. Try again later")
+  async function getImages() {
+    const {data} = await axios.get(`${API_URL}?query=${input}&page=${page}&client_id=${accessKey}`);
+    console.log(data.results)
+    setImages(data.results)
+    console.log('this ran')
+    setTotalPages(data.total_pages)
+  }
+
+  function nextPage() {
+    if(page < totalPages) {
+      setPage(page + 1)
     }
-  }, [page, input])
-
+    console.log('clicked show more')
+  }
+  function prevPage() {
+    if(page > 1) {
+      setPage(page - 1)
+    }
+  }
   useEffect(() => {
-    console.log("use effct ran")
-    fetchImages()
-  }, [fetchImages])
-
-  const imageDivs= images.map((item) => {
-    <div className='d-flex flex-column align-items-center p-3 col' style={{minWidth: '400px'}}>
-      <img src={item.urls.small} width='100%' />
-    </div>
-  })
+    getImages()
+  }, [page])
 
   return (
     <>
@@ -58,32 +55,25 @@ function App() {
             value={input}
           />
           <div className='text-end mt-2'>
-            <button className='btn btn-outline-primary'>Search</button>
+            <button className='btn btn-outline-primary' onClick={handleSearch}>Search</button>
           </div>
         </form>
         <section className='container row gap-3'>
           {
             images.map((image) => {
-              <img src={image.urls.small} />
+              return <img key={image.id} src={image.urls.small} />
             })
           }
-          <div className='d-flex flex-column align-items-center p-3 col' style={{minWidth: '400px'}}>
-            <img src='https://plus.unsplash.com/premium_photo-1691960547805-7143654bd06b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHx8&auto=format&fit=crop&w=800&q=60' width='100%' />
-            <a href='https://unsplash.com/photos/a-group-of-colorful-mushrooms-on-a-yellow-background-cgtZMRHZADM' target='_blank' >Mushrooms</a>
-          </div>
-          <div className='d-flex flex-column align-items-center p-3 col' style={{minWidth: '400px'}}>
-            <img src='https://plus.unsplash.com/premium_photo-1691960547805-7143654bd06b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHx8&auto=format&fit=crop&w=800&q=60' width='100%' />
-            <a href='https://unsplash.com/photos/a-group-of-colorful-mushrooms-on-a-yellow-background-cgtZMRHZADM' target='_blank' >Mushrooms</a>
-          </div>
-          <div className='d-flex flex-column align-items-center p-3 col' style={{minWidth: '400px'}}>
-            <img src='https://plus.unsplash.com/premium_photo-1691960547805-7143654bd06b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHx8&auto=format&fit=crop&w=800&q=60' width='100%' />
-            <a href='https://unsplash.com/photos/a-group-of-colorful-mushrooms-on-a-yellow-background-cgtZMRHZADM' target='_blank' >Mushrooms</a>
-          </div>
         </section>
       </main>
-      <div className='text-center'>
-        <button className='btn btn-primary'>Show more</button>
-      </div>
+      {
+        input && (
+        <div className='text-center m-3'>
+          <button className='btn btn-primary me-3' onClick={prevPage}>Prev Page</button>
+          <button className='btn btn-primary' onClick={nextPage}>Next Page</button>
+        </div>
+        )
+      }
     </>
   )
 }
